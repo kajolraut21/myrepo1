0f5d49a0a78e8a3190c0e0c09f628cac3c2f3d79
kajolraut21 <kajolraut21@gmail.com>
2021-09-24 11:55:39 +0530

    api_json

---
+++ API_JSON_TRY/script.js
@@ -0,0 +1,80 @@
+// let input = document.getElementById("entered");
+// input.addEventListener("keyup", function(event){
+//     if(event.code===Enter){
+//         myFunc()
+//     }
+// })
+function attempt(event){
+    console.log(event.key)
+    if(event.key=="Enter"){
+        console.log(event.key)
+        event.preventDefault();
+        myFunc()
+     
+    }
+}
+function myFunc(){
+    const api_url =
+	"http://numbersapi.com/";
+    const num =parseInt(document.getElementById("entered").value)
+    console.log(num)
+        const numb = api_url + num + "?json"
+        getapi(numb);
+}
+
+
+// 
+async function getapi(url) {
+	// Storing response
+	// const response = await fetch(url);	
+    fetch(url)
+.then(res => res.json()
+)
+.then(data => {
+    console.log(data)
+
+    document.getElementById("demo").innerHTML = data.text
+})
+.catch(error => {
+    console.log(error)
+    document.getElementById("demo").innerHTML = "COULD NOT FETCH"
+})
+}
+
+
+// function myFunc() {
+//     const num = parseInt(document.getElementById("entered").value)
+//     const api_url = "http://numbersapi.com/" + num + "/math?callback="
+
+//     var num1 = Math.round(10000 * Math.random())
+//     var call_back = "cb_" + num1
+//     console.log("1")
+
+//     window[call_back] = function(data){
+//         console.log("5")
+//         console.log(data)
+//         console.log("6")
+//     }
+
+
+//     var sc = document.createElement("script");
+//     sc.async = true;
+//     const numb = api_url + call_back
+//     console.log("2")
+//     sc.src = numb
+//     console.log("3")
+//     document.body.appendChild(sc);
+//     console.log(sc)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
